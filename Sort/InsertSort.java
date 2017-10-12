1465b4978ff29d410094d9f7501f17a8e6bbaba8
kyun91 <31693827+kyun91@users.noreply.github.com>
2017-10-12 12:49:44 +0900

    Sort

---
+++ Sort/InsertSort.java
@@ -0,0 +1,32 @@
+package Sort;
+
+public class InsertSort {
+	public void sort(int data[]){
+        int length = data.length;
+        int temp = 0;
+        int j = 0;
+        for(int i = 1; i < length; i++){
+            temp = data[i];
+            for(j=i-1; j>=0 && temp<data[j]; j--){
+                data[j+1] = data[j];
+            }
+            data[j+1] = temp;
+        }
+        
+        for(int i=0; i<length; i++){
+ 		   if(i>0){
+ 			   System.out.print("_");
+ 		   }
+ 		   System.out.print(data[i]);
+ 	   }
+    }
+	
+	
+	public static void main(String args[]){
+		SelectionSort insert = new SelectionSort();
+		   int data[] = {4,6,1,3,5,2};
+		   
+		   insert.sort(data);
+	}
+
+}
